f7a088a6853c30bdd31f7966e62fb865dfba51c0
valemoog <valentinadiaz@Valentinas-MacBook-Pro.local>
2017-06-05 23:32:41 -0500

    Final changes for hW

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-before_action :authenticate_user!
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:username])
+  end
+  before_action :authenticate_user!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
